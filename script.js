ab5aefc2b35429844e06280fad9d7f59e1108abe
M Waheed <waheed8@gmail.com>
2023-04-24 07:09:03 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+(async () => {
+    fetch("https://script.google.com/macros/s/AKfycbyApLpJPNU4kfc4nsYUu68oduqOiB8Min4795zU_GEIoyYIX5OwHP8vCth0s16ayjQw/exec")
+      .then(res => res.json())
+      .then(data => {
+        $(function() {
+          $("#dataset_select").selectize({
+            plugins: ["restore_on_backspace", "clear_button"],
+            maxItems: 1,
+            valueField: "name",
+            labelField: "name",
+            searchField: "name",
+            options: data
+          });
+        });
+  
+      })
+  
+    $("#dataset_select").bind("change", function(event) {
+      console.log("Dataset changed")
+      $("#output").empty().text("Loading...")
+      var val = $(this).val();
+      var renderers = $.extend(
+        $.pivotUtilities.renderers,
+        $.pivotUtilities.plotly_renderers
+      );
+      console.log("Fetching dataset...")
+      Papa.parse("https://data-browser.moe-mv.repl.co/datasets/" + val, {
+        download: true,
+        skipEmptyLines: true,
+        complete: function(parsed) {
+          $("#output").pivotUI(parsed.data, {
+            hiddenAttributes: [""],
+            renderers: renderers
+          }, true);
+        }
+      });
+    });
+  
+  })()
+  
